46bc7e8a1c2fc27b2d780f4011f1e4fefc646982
Satoshi <inabasatoshi@gmail.com>
2024-07-24 22:23:57 +0900

    メソッドを割り算に変更

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-def addtion(num1,num2):
-    answer = num1+num2
+def division(num1,num2):
+    answer = num1/num2
     return answer
 
 num1 = 1
 num2 = 2
 
-answer = addtion(num1,num2)
+answer = division(num1,num2)
 print(answer)
